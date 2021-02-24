5578b0610ebde5af410795d8f4633b5685835f87
Eason <eason@acumenonline.co.nz>
2021-02-25 09:13:59 +1300

    update the format settings

--- The-Daily-Byte-24-02-2021/script/main.js
+++ The-Daily-Byte-24-02-2021/script/main.js
@@ -6,10 +6,10 @@
 // "A man, a plan, a canal: Panama.", return true
 
 function palindrome(str) {
-    var re = /[\W_]/g;
-    var lowRegStr = str.toLowerCase().replace(re, '');
-    var reverseStr = lowRegStr.split('').reverse().join('');
-    return reverseStr === lowRegStr;
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
 }
 
 console.log(palindrome("level"));
@@ -18,15 +18,15 @@ console.log(palindrome("A man, a plan, a canal. Panama"));
 
 
 function palindromeTwo(str) {
-    var re = /[^A-Za-z0-9]/g;
-    str = str.toLowerCase().replace(re, '');
-    var len = str.length;
-    for (var i = 0; i < len / 2; i++) {
-        if (str[i] !== str[len - 1 - i]) {
-            return false;
-        }
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, '');
+  var len = str.length;
+  for (var i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 console.log(palindromeTwo("level"));
